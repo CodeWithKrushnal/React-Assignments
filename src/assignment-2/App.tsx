f724ffbe9e6839a6628a52f15393207c6a1f25a2
CodeWithKrushnal <krushnal.patil@joshsoftware.com>
2025-02-21 15:39:37 +0530

    Add Restructued Assignment 2

---
+++ src/assignment-2/App.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import {Resp} from '@/assignment-2/types/types'
+import TableContainer from "@/assignment-2/components/TableContainer.tsx";
+
+
+function App() {
+    const [data, setData] = useState({page: 0, per_page: 0, total: 0, total_pages: 0, data: [],} as Resp);
+
+    useEffect(() => {
+            const fetchData = async () => {
+                const response = await fetch("https://reqres.in/api/users?per_page=12");
+                const json: Resp = await response.json();
+                setData(json);
+            };
+            fetchData();
+        }, []
+    )
+
+    return (
+        < div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="scroll-m-20  text-3xl font-semibold tracking-tight first:mt-0 pb-10 pt-10 items-center">React
+                UseEffect Hook Demo</h1>
+            <div className="max-w-fit border-2 border-gray-600 rounded-xl shadow-lg shadow-cyan-500/50">
+                <TableContainer data={data}/>
+            </div>
+        </div>
+    )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assignment-1/index.css'
-import App from './assignment-1/App.tsx'
+import './assignment-2/index.css'
+import App from './assignment-2/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
