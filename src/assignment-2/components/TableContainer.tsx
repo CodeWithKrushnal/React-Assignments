7d5153d50b1b82c25894ae936f39ffd8e6f2eee2
CodeWithKrushnal <krushnal.patil@joshsoftware.com>
2025-02-24 09:29:10 +0530

    Add Suggested Changes Regarding Creation on util functions for CRUD API Calls and Containers by @Rushikesh-josh

--- src/assignment-2/components/TableContainer.tsx
+++ src/assignment-2/components/TableContainer.tsx
@@ -1,39 +1,49 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow
-} from "@/assignment-2/components/ui/table.tsx";
 import {Resp, User} from "@/assignment-2/types/types.ts";
-import TableContents from "@/assignment-2/components/TableContents.tsx";
+import {JSX, useEffect, useState} from "react";
+import {fetchUsers} from "@/assignment-2/services/userService.ts";
+import TableStructure from "@/assignment-2/components/ui/TableStructure.tsx";
+import SearchForm from "@/assignment-2/components/SearchForm.tsx";
 
-const TableContainer = ({data}: { data: Resp }) => {
-    return (<Table>
-        <TableCaption className="pb-5">A Demo For React UseEffect Hook</TableCaption>
-        <TableHeader>
-            <TableRow>
-                <TableHead className="text-center py-3">ID</TableHead>
-                <TableHead className="text-center py-3">Avatar</TableHead>
-                <TableHead className="text-center py-3">First Name</TableHead>
-                <TableHead className="text-center py-3">Last Name</TableHead>
-                <TableHead className="text-center py-3">Email</TableHead>
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-            {data.data.map((unit: User) => (
-                <TableContents unit={unit}/>
-            ))}
-        </TableBody>
-        <TableFooter>
-            <TableRow>
-                <TableCell colSpan={5}>Total Users: {data.total}</TableCell>
-            </TableRow>
-        </TableFooter>
-    </Table>)
+const TableContainer = (): JSX.Element => {
+
+    const [data, setData] = useState({page: 0, per_page: 0, total: 0, total_pages: 0, data: [],} as Resp);
+    const [displayData, setDisplayData] = useState({page: 0, per_page: 0, total: 0, total_pages: 0, data: []} as Resp);
+
+    useEffect(() => {
+            const getUsers = async () => {
+                try {
+                    const data: Resp = await fetchUsers();
+                    setData(data);
+                    setDisplayData(data)
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            getUsers();
+        }, []
+    )
+
+    // Custom Hook
+    const useSearch = (event: React.KeyboardEventHandler<HTMLInputElement>): void => {
+        console.log(event);
+        const res = data.data.filter((unit: User): boolean => unit.first_name.toLowerCase().includes(event.target.value.toLowerCase()) || unit.last_name.toLowerCase().includes(event.target.value.toLowerCase()) || unit.email.toLowerCase().includes(event.target.value.toLowerCase()) || unit.id.toString().includes(event.target.value));
+        setDisplayData({
+            page: data.page,
+            per_page: data.per_page,
+            total: data.total,
+            total_pages: data.total_pages,
+            data: res
+        })
+    }
+
+    return (<>
+        <div className="text-center justify-center flex w-full items-center space-x-2 pb-10">
+            <SearchForm useSearch={useSearch}/>
+        </div>
+        <div className="min-w-xl border-2 border-gray-600 rounded-xl shadow-lg shadow-cyan-500/50">
+            <TableStructure data={displayData}/>
+        </div>
+    </>)
 }
 
 export default TableContainer;
--- src/assignment-2/components/TableContents.tsx
+++ src/assignment-2/components/TableContents.tsx
@@ -2,17 +2,17 @@ import {TableCell, TableRow} from "@/assignment-2/components/ui/table.js";
 import {Avatar, AvatarFallback, AvatarImage} from "@/assignment-2/components/ui/avatar.js";
 import {User} from "@/assignment-2/types/types.ts";
 
-function TableContents({unit}:{unit:User}) {
+function TableContents({unit}: { unit: User }) {
     return (
-        <TableRow key={unit.id}>
-            <TableCell className="text-center px-5">{unit.id}</TableCell>
+        <TableRow key={unit?.id}>
+            <TableCell className="text-center px-5">{unit?.id}</TableCell>
             <TableCell className="text-center px-5"> <Avatar>
-                <AvatarImage src={unit.avatar} alt={unit.first_name}/>
+                <AvatarImage src={unit?.avatar} alt={unit?.first_name}/>
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar></TableCell>
-            <TableCell className="text-center px-5">{unit.first_name}</TableCell>
-            <TableCell className="text-center px-5">{unit.last_name}</TableCell>
-            <TableCell className="text-center px-5">{unit.email}</TableCell>
+            <TableCell className="text-center px-5">{unit?.first_name}</TableCell>
+            <TableCell className="text-center px-5">{unit?.last_name}</TableCell>
+            <TableCell className="text-center px-5">{unit?.email}</TableCell>
         </TableRow>
     )
 }
